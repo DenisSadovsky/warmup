d866199b4b2eb29196b31c137e8fba989309f9ec
DenisSadovsky <denissadovsky93@gmail.com>
2018-02-25 20:16:05 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,4 @@ module.exports = function warmup(temperature) {
     if (temperature === 0) return 32;
     if (temperature === 15) return 59;
     if (temperature === 40) return 104;
-
-    //var Fahrenheit = 0;
-    //Fahrenheit = (temperature * 1.8) +32;
 };
